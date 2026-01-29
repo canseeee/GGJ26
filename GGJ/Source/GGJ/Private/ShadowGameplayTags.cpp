23ef13d99f910e3ec4327654d19e57d38b039060
Prospect <1693419288@qq.com>
2026-01-29 19:27:25 +0800

    GameplayTag

---
+++ GGJ/Source/GGJ/Private/ShadowGameplayTags.cpp
@@ -0,0 +1,19 @@
+#include "ShadowGameplayTags.h"
+
+UE_DEFINE_GAMEPLAY_TAG(InputTag_LMB, "InputTag.LMB");
+UE_DEFINE_GAMEPLAY_TAG(InputTag_RMB, "InputTag.RMB");
+UE_DEFINE_GAMEPLAY_TAG(InputTag_Shift, "InputTag.Shift");
+UE_DEFINE_GAMEPLAY_TAG(InputTag_Q, "InputTag.Q");
+UE_DEFINE_GAMEPLAY_TAG(InputTag_E, "InputTag.E");
+UE_DEFINE_GAMEPLAY_TAG(InputTag_R, "InputTag.R");
+
+UE_DEFINE_GAMEPLAY_TAG(State_Locker_Attacking,"State.Locker.Attacking");
+UE_DEFINE_GAMEPLAY_TAG(State_Debuff_Sprinting,"State.Locker.Sprinting");
+UE_DEFINE_GAMEPLAY_TAG(State_Debuff_Stunned,"State.Debuff.Stunned");
+UE_DEFINE_GAMEPLAY_TAG(State_Status_HitReact, "State.Status.HitReact");
+UE_DEFINE_GAMEPLAY_TAG(State_Status_Invincible, "State.Status.Invincible");
+UE_DEFINE_GAMEPLAY_TAG(State_Status_Dead, "State_Status.Dead");
+
+UE_DEFINE_GAMEPLAY_TAG(Ability_Weapon_Dao_MeleeAttack_X, "Ability.Weapon_Dao.MeleeAttack.X");
+UE_DEFINE_GAMEPLAY_TAG(Ability_Weapon_Dao_MeleeAttack_XX, "Ability.Weapon_Dao.MeleeAttack.XX");
+UE_DEFINE_GAMEPLAY_TAG(Ability_Weapon_Dao_MeleeAttack_XXX, "Ability.Weapon.Dao.MeleeAttack.XXX");
